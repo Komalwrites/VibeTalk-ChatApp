d379894134cd5cbfebb2cf41ea573d5eb44a473a
The Komal <komalcomal811@gmail.com>
2025-02-08 16:18:29 +0530

    Project completed

--- server/index.js
+++ server/index.js
@@ -2,28 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
-import {connectDB} from "./lib/db.js"
-import { app, server} from "./lib/socket.js"
 
-import authRoutes from "./routes/authRoutes.js"
-import messageRoutes from "./routes/messageRoutes.js"
+import { connectDB } from "./lib/db.js";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/auth", authRoutes);
 app.use("/messages", messageRoutes);
-
-const PORT = process.env.PORT;
-const __dirname = path.resolve;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
